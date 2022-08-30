f56c047b3e48eded5709db21b52227fc31b80ac1
ACoolName <ACoolName9001@gmail.com>
2022-08-30 20:38:47 +0300

    fixed delete

--- cogs/minecraft_loader.py
+++ cogs/minecraft_loader.py
@@ -53,12 +53,12 @@ class MinecraftCommands(commands.Cog):
         userid = interaction.user.id
         containers = self.docker.containers.list(all=True, filters={'name': self.format_container_name(userid, game)})
         if len(containers) == 0:
-            await interaction.response.send_message(f'You do not have a server of game {await self.get_display_name_from_container_name(self.format_container_name(game), with_username=False)}', ephemeral=True)
+            await interaction.response.send_message(f'You do not have a server of game {await self.get_display_name_from_container_name(self.format_container_name(userid, game), with_username=False)}', ephemeral=True)
             return
         try:
             for container in containers:
                 container.remove(force=True)
-            await interaction.response.send_message(f'Deleted game {await self.get_display_name_from_container_name(self.format_container_name(game), with_username=False)}', ephemeral=True)
+            await interaction.response.send_message(f'Deleted game {await self.get_display_name_from_container_name(self.format_container_name(userid, game), with_username=False)}', ephemeral=True)
         except Exception as e:
             logging.error(f'Failed to delete container: {e}', exc_info=True)
             await interaction.response.send_message(f'Failed to delete server please try again later.', ephemeral=True)
