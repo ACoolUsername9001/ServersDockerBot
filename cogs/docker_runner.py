6cd69eb30dec015cb9c512118e1e403d111c8d32
ACoolName <ACoolName9001@gmail.com>
2022-09-04 01:36:22 +0300

    reformatted file and change server start logic

---
+++ cogs/docker_runner.py
@@ -0,0 +1,236 @@
+import os
+import re
+import time
+from collections import defaultdict
+
+import docker
+from typing import Optional, List, Dict, Tuple, Any
+from docker.models.containers import Container
+from docker.models.images import Image
+from docker.models.volumes import Volume
+from docker.types import Mount
+
+GAMES_REPOSITORY = 'games'
+FILE_BROWSER_PREFIX = 'filebrowser'
+FILE_BROWSER_IMAGE = 'filebrowser/filebrowser'
+
+ansi_escape = re.compile(br'(?:\x1B[@-Z\\-_]|[\x80-\x9A\x9C-\x9F]|(?:\x1B\[|\x9B)[0-?]*[ -/]*[@-~])')
+
+ports_format = re.compile(r'(?P<port>\d+)(?:/(?P<protocol>\w+))')
+
+
+class GameNotFound(Exception):
+    pass
+
+
+class ServerNotFound(Exception):
+    pass
+
+
+class ServerAlreadyRunning(Exception):
+    pass
+
+
+class ServerNotRunning(Exception):
+    pass
+
+
+class DockerRunner:
+
+    def __init__(self, docker_client: Optional[docker.DockerClient] = None,
+                 games_repository: str = GAMES_REPOSITORY,
+                 games_prefix: str = GAMES_REPOSITORY,
+                 filebrowser_prefix: str = FILE_BROWSER_PREFIX,
+                 filebrowser_repository: str = FILE_BROWSER_IMAGE):
+        if not docker_client:
+            docker_client = docker.from_env()
+        self._games_repository = games_repository
+        self._games_prefix = games_prefix
+        self._filebrowser_prefix = filebrowser_prefix
+        self.docker = docker_client
+        self.docker.images.pull(repository=filebrowser_repository)
+        self._filebrowser_image = filebrowser_repository
+
+    @staticmethod
+    def get_user_id_and_image_name_from_game_server_name(server_name):
+        match = re.match(r'(?P<userid>\d+)-(?P<server>.+)', server_name)
+        groups = match.groupdict()
+        return groups.get('userid'), groups.get('server')
+
+    def _hide_games_prefix(self, name: str):
+        return name[len(self._games_prefix)+1:]
+
+    def _hide_file_browser_prefix(self, name):
+        return name[len(self._filebrowser_prefix)+1:]
+
+    def _format_game_container_name(self, user_id=None, game=None) -> str:
+        if not game:
+            if not user_id:
+                return f'{self._games_prefix}-'
+            return f'{self._games_prefix}-{user_id}-'
+        return f'{self._games_prefix}-{user_id}-{game}'
+
+    def _format_file_browser_container_name(self, user_id) -> str:
+        return f'{self._filebrowser_prefix}-{user_id}'
+
+    def _format_image_name(self, tag):
+        return f'{self._games_repository}:{tag}'
+
+    def _list_server_volumes(self, user_id=None, prefix: Optional[str] = None) -> List[Volume]:
+        return self.docker.volumes.list(filters={'name': self._format_game_container_name(user_id=user_id, game=prefix)})
+
+    def _list_running_server_containers(self, user_id=None, prefix: Optional[str] = None) -> List[Container]:
+        return self.docker.containers.list(filters={'name': self._format_game_container_name(user_id=user_id, game=prefix)})
+
+    def _list_file_browsers(self, user_id) -> List[Container]:
+        return self.docker.containers.list(filters={'name': self._format_file_browser_container_name(user_id=user_id)})
+
+    def _list_game_images(self) -> List[Image]:
+        return self.docker.images.list(all=True, name=self._games_repository)
+
+    def list_server_names(self, user_id=None, prefix: Optional[str] = None) -> List[str]:
+        return [self._hide_games_prefix(v.name) for v in self._list_server_volumes(user_id, prefix)]
+
+    def list_running_server_names(self, user_id=None, prefix: Optional[str] = None) -> List[str]:
+        return [self._hide_games_prefix(c.name) for c in self._list_running_server_containers(user_id=user_id, prefix=prefix)]
+
+    def list_stopped_server_names(self, user_id: Optional[Any] = None, prefix: Optional[str] = None) -> List[str]:
+        servers = self.list_server_names(user_id=user_id, prefix=prefix)
+        running_servers = self.list_running_server_names(user_id=user_id, prefix=prefix)
+        return [server for server in servers if server not in running_servers]
+
+    def list_file_browser_names(self, user_id) -> List[str]:
+        return [self._hide_file_browser_prefix(c.name) for c in self._list_file_browsers(user_id=user_id)]
+
+    def list_game_names(self) -> List[str]:
+        tags = []
+        for image in self._list_game_images():
+            tags.extend(x.split(':')[1] for x in image.tags)
+        return tags
+
+    def create_game_server(self, user_id, game: str) -> str:
+        game_images = self.list_game_names()
+
+        if game not in game_images:
+            raise GameNotFound(f'Game {game} was not found')
+
+        return self.docker.volumes.create(name=self._format_game_container_name(user_id=user_id, game=game)).name
+
+    def _get_server_image_working_dir(self, image_tag):
+        image_name = self._format_image_name(image_tag)
+        image = self.docker.images.get(image_name)
+        return image.attrs.get('Config', {}).get('WorkingDir')
+
+    @staticmethod
+    def _find_suitable_ports(ports: List[str]) -> Dict[str, Optional[str]]:
+        # port_mapping = defaultdict(list)
+        # for port in ports:
+        #     match = ports_format.match(port).groupdict()
+        #     port = match.get('port')
+        #     protocol = match.get('protocol', 'tcp')
+        #     port_mapping[port].append(protocol)
+        #
+        # for port, protocols in port_mapping.items():
+        #       TODO: add custom port finding logic
+        #
+        return {port: None for port in ports}
+
+    @staticmethod
+    def get_ports_from_container(container) -> List[str]:
+        available_ports = []
+        for key, value in container.ports.items():
+            protocol = key.split('/')[-1]
+            if value:
+                host_ports = [v['HostPort'] for v in value]
+                available_ports.extend(f'{port}/{protocol}' for port in host_ports)
+        return available_ports
+
+    def start_game_server(self, game, ports: List[str], command_parameters: Optional[str] = None) -> List[str]:
+        user_id, image_name = self.get_user_id_and_image_name_from_game_server_name(game)
+        if image_name not in self.list_game_names():
+            raise GameNotFound(f'Game {image_name} was not found')
+
+        all_servers = self.list_server_names(user_id=user_id, prefix=image_name)
+        if len(all_servers) == 0:
+            raise ServerNotFound(f'Server of game {game} was not found')
+
+        running_servers = self.list_running_server_names(user_id=user_id, prefix=game)
+        if len(running_servers) > 1:
+            raise ServerAlreadyRunning(f'Server of game {game} is already running')
+
+        working_dir = self._get_server_image_working_dir(image_name)
+        server_name = self._format_game_container_name(user_id=user_id, game=image_name)
+        if working_dir:
+            mount = [Mount(target=working_dir, source=server_name, type='volume')]
+        else:
+            mount = None
+
+        ports = self._find_suitable_ports(ports)
+
+        container = self.docker.containers.create(image=self._format_image_name(image_name),
+                                                  name=server_name,
+                                                  mounts=mount,
+                                                  command=command_parameters,
+                                                  ports=ports,
+                                                  stdin_open=True,
+                                                  tty=True,
+                                                  auto_remove=True)
+        container.start()
+        time.sleep(0.01)
+        container = self.docker.containers.get(container_id=container.id)
+        return self.get_ports_from_container(container)
+
+    def run_command(self, server, command) -> str:
+        try:
+            user_id, image_name = self.get_user_id_and_image_name_from_game_server_name(server_name=server)
+            container = self.docker.containers.get(self._format_game_container_name(user_id=user_id, game=image_name))
+        except Exception as e:
+            raise ServerNotRunning(e)
+
+        sin = container.attach_socket(params={'stdin': True, 'stream': True, 'stdout': True, 'stderr': True})
+
+        os.write(sin.fileno(), f'{command}\n'.encode('utf-8'))
+        with open(sin.fileno(), 'rb') as f:
+            while command in (r := ansi_escape.sub(b'', f.readline()).decode().replace('\n', '').replace('\r', '')) or not r or r == '>':
+                pass
+        sin.close()
+
+        return r
+
+    def delete_game_server(self, user_id, game):
+        server = self._format_game_container_name(user_id=user_id, game=game)
+        try:
+            volume = self.docker.volumes.get(server)
+        except Exception as e:
+            raise ServerNotFound(e)
+
+        for container in self._list_running_server_containers(user_id=user_id, prefix=game):
+            container.remove(force=True)
+
+        for browser in self._list_file_browsers(user_id=user_id):
+            mounts = browser.attrs.get('Mounts')
+            for mount in mounts:
+                if mount.get('Name') == server:
+                    browser.remove(force=True)
+                    break
+
+        volume.remove(force=True)
+
+    def start_file_browser(self, user_id, server) -> List[str]:
+        container_name = self._format_game_container_name(user_id=user_id, game=server)
+        mounts = [Mount(source=container_name, target='/tmp/data', type='volume')]
+        file_browser_name = self._format_file_browser_container_name(user_id=user_id)
+        if len(self.list_file_browser_names(user_id=user_id)) > 1:
+            raise ServerAlreadyRunning()
+        container = self.docker.containers.create(image=self._filebrowser_image, name=file_browser_name, auto_remove=True, command='-r /tmp/data', mounts=mounts, ports={'80/tcp': None})
+        container.start()
+        time.sleep(0.01)
+        container = self.docker.containers.get(container_id=container.id)
+        available_ports = self.get_ports_from_container(container)
+        return available_ports
+
+    def stop_file_browsing(self, user_id):
+        file_browsers = self._list_file_browsers(user_id=user_id)
+        for file_browser in file_browsers:
+            file_browser.stop()
+
--- cogs/minecraft_loader.py
+++ cogs/minecraft_loader.py
@@ -10,65 +10,26 @@ from discord import app_commands, Interaction
 from discord.app_commands import Choice
 from discord.ext import commands
 
-SERVERS_LIMIT = 5
-GAMES_REPOSITORY = 'games'
-FILE_BROWSER_PREFIX = 'filebrowser'
-FILE_BROWSER_IMAGE = 'filebrowser/filebrowser'
-ansi_escape = re.compile(br'(?:\x1B[@-Z\\-_]|[\x80-\x9A\x9C-\x9F]|(?:\x1B\[|\x9B)[0-?]*[ -/]*[@-~])')
+from cogs.docker_runner import DockerRunner
 
 
 class MinecraftCommands(commands.Cog):
 
-    def __init__(self, bot: commands.Bot, docker_client: docker.client.DockerClient = None, **kwargs):
-        if not docker_client:
-            docker_client = docker.from_env()
-        self.docker = docker_client
+    def __init__(self, bot: commands.Bot, docker_runner: DockerRunner = None, **kwargs):
+        if not docker_runner:
+            docker_runner = DockerRunner()
+        self.docker = docker_runner
         self.bot = bot
         super().__init__(**kwargs)
-
-    @staticmethod
-    def format_game_container_name(userid, game=None) -> str:
-        if not game:
-            return f'{GAMES_REPOSITORY}-{userid}'
-        return f'{GAMES_REPOSITORY}-{userid}-{game}'
-
-    @staticmethod
-    def format_file_browser_container_name(userid) -> str:
-        return f'{FILE_BROWSER_PREFIX}-{userid}'
 
     @app_commands.command(name='create', description='This will create a new minecraft server')
     @app_commands.checks.has_permissions(administrator=True)
     @app_commands.guilds(1013092707494809700)
-    @app_commands.describe(game='Game Server',
-                           server_ports='Space seperated list of ports the server is listening on',
-                           command_parameters='Optional parameters to pass to the server')
-    async def create(self, interaction: Interaction, game: str, server_ports: str, command_parameters: Optional[str] = None):
+    @app_commands.describe(game='Game Server')
+    async def create(self, interaction: Interaction, game: str):
         userid = interaction.user.id
-        if len(self.docker.containers.list(all=True, filters={'name': self.format_game_container_name(userid)})) > SERVERS_LIMIT:
-            await interaction.response.send_message(f'You have already created a server, each user is limited to {SERVERS_LIMIT} servers', ephemeral=True)
-            return
-        container_name = self.format_game_container_name(userid, game)
-        if len(self.docker.containers.list(all=True, filters={'name': container_name})) > 0:
-            await interaction.response.send_message(f'You have already created a server of that version, limited to 1 per user', ephemeral=True)
-            return
         try:
-            image_name = f'{GAMES_REPOSITORY}:{game}'
-            image = self.docker.images.get(image_name)
-            working_dir = image.attrs.get('Config', {}).get('WorkingDir')
-
-            if working_dir:
-                mounts = [Mount(source=container_name, target=working_dir, type='volume')]
-            else:
-                mounts = None
-
-            self.docker.containers.create(image=image_name,
-                                          name=container_name,
-                                          stdin_open=True,
-                                          ports={server_port: None for server_port in server_ports.split()},
-                                          tty=True,
-                                          command=command_parameters,
-                                          mounts=mounts)
-
+            self.docker.create_game_server(user_id=userid, game=game)
             await interaction.response.send_message(f'Created server {game.replace("-", " ").title()}', ephemeral=True)
         except Exception as e:
             logging.error(f'Failed to create container: {e}', exc_info=True)
@@ -80,16 +41,7 @@ class MinecraftCommands(commands.Cog):
     @app_commands.describe(game='The game server to browse it\'s files')
     async def start_browsing(self, interaction: Interaction, game: str):
         user_id = interaction.user.id
-        container_name = self.format_game_container_name(userid=user_id, game=game)
-        mounts = [Mount(source=container_name, target='/tmp/data', type='volume')]
-        file_browser_name = self.format_file_browser_container_name(userid=user_id)
-        if self.docker.containers.list(filters=dict(name=file_browser_name)):
-            await interaction.response.send_message(f'You already have a file browser open', ephemeral=True)
-            return
-        container = self.docker.containers.create(image=FILE_BROWSER_IMAGE, name=file_browser_name, auto_remove=True, command='-r /tmp/data', mounts=mounts, ports={'80/tcp': None})
-        container.start()
-        container = self.docker.containers.get(container_id=container.id)
-        available_ports = self.get_ports_from_container(container)
+        available_ports = self.docker.start_file_browser(user_id=user_id, server=game)
         await interaction.response.send_message(f'Opened file browser on ports {",".join(available_ports)}', ephemeral=True)
 
     @app_commands.command(name='stop-browsing-files', description='Stops the file browser')
@@ -97,9 +49,7 @@ class MinecraftCommands(commands.Cog):
     @app_commands.guilds(1013092707494809700)
     async def stop_browsing(self, interaction: Interaction):
         user_id = interaction.user.id
-        container_name = self.format_file_browser_container_name(userid=user_id)
-        container = self.docker.containers.get(container_id=container_name)
-        container.stop()
+        self.docker.stop_file_browsing(user_id=user_id)
         await interaction.response.send_message('Stopped file browser', ephemeral=True)
 
     @app_commands.command(name='delete', description='This will create a new minecraft server')
@@ -108,21 +58,9 @@ class MinecraftCommands(commands.Cog):
     @app_commands.describe(game='Game Server')
     async def delete(self, interaction: Interaction, game: str):
         userid = interaction.user.id
-        container_name = self.format_game_container_name(userid, game)
-        containers = self.docker.containers.list(all=True, filters={'name': container_name})
-        if len(containers) == 0:
-            await interaction.response.send_message(f'You do not have a server of game {await self.get_display_name_from_container_name(container_name, with_username=False)}', ephemeral=True)
-            return
         try:
-            for container in containers:
-                container.remove(force=True)
-                try:
-                    volume = self.docker.volumes.get(container.name)
-                    volume.remove(force=True)
-                except NotFound:
-                    pass
-
-            await interaction.response.send_message(f'Deleted game {await self.get_display_name_from_container_name(container_name, with_username=False)}', ephemeral=True)
+            self.docker.delete_game_server(user_id=userid, game=game)
+            await interaction.response.send_message(f'Deleted game {await self.format_display_name(server_name=game)}', ephemeral=True)
         except Exception as e:
             logging.error(f'Failed to delete container: {e}', exc_info=True)
             await interaction.response.send_message(f'Failed to delete server please try again later.', ephemeral=True)
@@ -132,20 +70,8 @@ class MinecraftCommands(commands.Cog):
     @app_commands.guilds(1013092707494809700)
     @app_commands.describe(command='command to run')
     async def run_command(self, interaction: Interaction, game: str, command: str):
-        containers = self.docker.containers.list(filters={'name': self.format_game_container_name(game)})
-        if not len(containers) == 1:
-            await interaction.response.send_message(f'{await self.get_display_name_from_container_name(game)} is not running')
-            return
-
-        container = containers[0]
-        sin = container.attach_socket(params={'stdin': True, 'stream': True, 'stdout': True, 'stderr': True})
-
-        os.write(sin.fileno(), f'{command}\n'.encode('utf-8'))
-        with open(sin.fileno(), 'rb') as f:
-            while command in (r := ansi_escape.sub(b'', f.readline()).decode().replace('\n', '').replace('\r', '')) or not r or r == '>':
-                pass
-        sin.close()
-        await interaction.response.send_message(r[:2000])
+        response = self.docker.run_command(game, command)
+        await interaction.response.send_message(response[:2000])
 
     @commands.command(name='sync')
     async def sync(self, ctx: commands.Context, guild: Optional[discord.Guild] = None):
@@ -154,62 +80,63 @@ class MinecraftCommands(commands.Cog):
     @app_commands.command(name='start')
     @app_commands.checks.has_permissions(administrator=True)
     @app_commands.guilds(1013092707494809700)
-    @app_commands.describe(game='What kind of server to start')
-    async def start_container(self, interaction: discord.Interaction, game: str):
-        containers = self.docker.containers.list(all=True, filters={'name': self.format_game_container_name(game)})
-        if not containers:
-            return
-        container = containers[0]
-        container.start()
-        container = self.docker.containers.get(container_id=container.id)
-        available_ports = self.get_ports_from_container(container)
-        await interaction.response.send_message(f'Starting {await self.get_display_name_from_container_name(container.name)} on port(s): {",".join(available_ports)}')
-
-    @staticmethod
-    def get_ports_from_container(container):
-        available_ports = []
-        for key, value in container.ports.items():
-            protocol = key.split('/')[-1]
-            if value:
-                host_ports = [v['HostPort'] for v in value]
-                available_ports.extend(f'{port}/{protocol}' for port in host_ports)
-        return available_ports
+    @app_commands.describe(game='What kind of server to start',
+                           server_ports='Space seperated list of ports the server is listening on',
+                           command_parameters='Optional parameters to pass to the server')
+    async def start_container(self, interaction: discord.Interaction, game: str, server_ports: str, command_parameters: Optional[str] = None):
+        ports = server_ports.split()
+        available_ports = self.docker.start_game_server(game=game, ports=ports, command_parameters=command_parameters)
+        user_id, server = self.docker.get_user_id_and_image_name_from_game_server_name(game)
+        await interaction.response.send_message(f'Starting {await self.format_display_name(user_id=user_id, server_name=server)} on port(s): {",".join(available_ports)}')
 
     @create.autocomplete('game')
     async def auto_complete_all_images(self, interaction: Interaction, current: str):
-        games = self.docker.images.list(all=True, name=GAMES_REPOSITORY)
-        choices = []
-        for game in games:
-            choices.extend([Choice(name=tag.split(':')[1].replace('-', ' ').title(), value=tag.split(':')[1]) for tag in game.tags if tag.startswith(current)])
-        return choices
+        games = self.docker.list_game_names()
+        return [Choice(name=await self.format_display_name(server_name=game), value=game) for game in games if game.startswith(current)]
 
-    async def get_display_name_from_container_name(self, container_name, with_username=True):
-        container_parts = container_name.split('-')[1:]
-        user_id = container_parts[0]
-        user = await self.bot.fetch_user(int(user_id))
+    async def format_display_name(self, server_name, user_id: Optional[int] = None):
+        new_container_name = server_name.replace('-', ' ').title()
 
-        new_container_name = ' '.join(container_parts[1:]).title()
-        if with_username:
+        if user_id:
+            user = await self.bot.fetch_user(int(user_id))
             return f'{user.name}#{user.discriminator}\'s {new_container_name}'
-        else:
-            return new_container_name
+
+        return new_container_name
 
     @start_container.autocomplete('game')
     async def autocomplete_all_containers(self, interaction: Interaction, current: str):
-        games = self.docker.containers.list(all=True, filters={'name': self.format_game_container_name(current)})
-        return [Choice(name=await self.get_display_name_from_container_name(game.name), value='-'.join(game.name.split('-')[1:])) for game in games]
+        games = self.docker.list_stopped_server_names()
+        choices = []
+        for game in games:
+            user_id, server = self.docker.get_user_id_and_image_name_from_game_server_name(game)
+            display_name = await self.format_display_name(user_id=user_id, server_name=server)
+            if current.lower() in display_name.lower():
+                choices.append(Choice(name=display_name, value=game))
+        return choices
 
     @delete.autocomplete('game')
     @start_browsing.autocomplete('game')
     async def autocomplete_user_containers(self, interaction: Interaction, current: str):
         userid = interaction.user.id
-        games = self.docker.containers.list(all=True, filters={'name': self.format_game_container_name(userid, current)})
-        return [Choice(name=await self.get_display_name_from_container_name(game.name, with_username=False), value='-'.join(game.name.split('-')[2:])) for game in games]
+        games = self.docker.list_server_names(user_id=userid)
+        choices = []
+        for game in games:
+            user_id, server = self.docker.get_user_id_and_image_name_from_game_server_name(game)
+            display_name = await self.format_display_name(user_id=user_id, server_name=server)
+            if current.lower() in display_name.lower():
+                choices.append(Choice(name=display_name, value=server))
+        return choices
 
     @run_command.autocomplete('game')
     async def autocomplete_user_active_containers(self, interaction: Interaction, current: str):
-        games = self.docker.containers.list(filters={'name': self.format_game_container_name(current)})
-        return [Choice(name=await self.get_display_name_from_container_name(game.name), value='-'.join(game.name.split('-')[1:])) for game in games]
+        games = self.docker.list_running_server_names()
+        choices = []
+        for game in games:
+            user_id, server = self.docker.get_user_id_and_image_name_from_game_server_name(game)
+            display_name = await self.format_display_name(user_id=user_id, server_name=server)
+            if current.lower() in display_name.lower():
+                choices.append(Choice(name=display_name, value=game))
+        return choices
 
 
 async def setup(bot: commands.Bot):
