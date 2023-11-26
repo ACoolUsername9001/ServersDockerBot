e3c6d17f888e00862ffc203c4ad2112bfe8d2e9e
acoolname <acoolname@acooldomain.co>
2023-11-26 20:31:33 +0200

    fixed api

--- api_code/database/crud.py
+++ api_code/database/crud.py
@@ -38,14 +38,12 @@ def delete_user(db: Session, username: str):
 
 
 def change_permissions(db: Session, username: str, permissions: list[models.Permission]):
-    db_user = db.query(models.DatabaseUser).filter(models.DatabaseUser.username == username).first()
-    if db_user is None:
-        return
-    
-    new_user = models.DatabaseUser(email=db_user.email, password_hash=db_user.password_hash, username=db_user.username, scope=':'.join(permissions))
-    db.refresh(new_user)
+    db_user = db.query(models.DatabaseUser).filter(models.DatabaseUser.username == username)
+
+    db_user.update({'scope': ':'.join(permissions)})
+    # db.query(models.DatabaseUser).filter(models.DatabaseUser.username == username).delete()
     db.commit()
-    return models.UserBase.from_database_user(new_user)
+    return models.UserBase.from_database_user(db_user.first())
 
 
 def create_user_from_token(db: Session, token: str, username: str, password_hash: str):
